006e23a28c574e0295ab4776cad3656c0b641a60
xzc <230616964@qq.com>
2025-06-03 19:35:09 +0800

    done with lab03

--- lab03/Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ lab03/Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,49 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private int totalAdded;
+    private IntegerList list;
 
+    public DelegationSortedIntList() {
+        this.totalAdded = 0;
+        this.list = new SortedIntList();
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
+
+    @Override
+    public boolean add(int num) {
+        this.totalAdded++;
+        return list.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        this.totalAdded += list.size();
+        return this.list.addAll(list);
+    }
+
+    @Override
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return list.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return this.list.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
 }
--- lab03/Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ lab03/Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,33 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int totalAdded;
 
+    public InheritanceSortedIntList() {
+        super();
+        totalAdded = 0;
+    }
+
+    /**
+     * Returns the count of the number of attempted element insertions
+     * 
+     * @return the count of the number of attempted element insertions
+     */
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
+
+    /**
+     * Adds the specified int to the list.
+     * Updates the count of the number of attempted element insertions.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    public boolean add(int num) {
+        this.totalAdded++;
+        return super.add(num);
+    }
 }
